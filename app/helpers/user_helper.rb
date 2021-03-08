57adcbffb145e3c9a73cc00cd71bb4f832df2098
wandji20 <wbertrand10@gmail.com>
2021-03-08 12:01:45 +0100

    Add check for sent_friendship request

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
-  # def friend
-  #   current_user.confirmed_friendships
-  # end
+  def sent_request_to(user)
+    current_user.friendships.find_by(user_id: current_user.id, friend_id: user.id )
+  end
 
   def confirmed_friend(user)
     friendship = friend(user)
@@ -11,8 +11,6 @@ module UserHelper
   def invite_friendship(user)
     link = link_to('Invite to friendship', friendships_path(friend_id: user),
                    method: :post, class: 'btn btn-info w-25')
-    link if !current_user.pending_friends.include?(user) && !current_user.friends.include?(user) && !current_user.unconfirmed_friends
+    link if !current_user.pending_friends.include?(user) && !current_user.friends.include?(user) && !sent_request_to(user)
   end
 end
-
-# link should show if user is not friend with user on the list or if he has a pending request from  user in  the list
