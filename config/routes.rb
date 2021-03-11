d9b7e0d0e5164d2063ba539f8fe179284dc9944b
helciodev <helcioandre@live.com>
2021-03-11 09:49:27 +0100

    Move pg gem to development and test and delete unused line of code requiring simplecov gem

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :friendships 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
-    resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+  resources :comments, only: [:create]
+  resources :likes, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
